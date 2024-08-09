9f1c3c57f06dc0a99fff7e46383eb33529a63f08
luest10 <luestmon@gmail.com>
2024-08-08 23:07:47 -0300

    first commit

---
+++ views/auth/olvide-password.php
@@ -0,0 +1,25 @@
+<h1 class="nombre-pagina">Olvide Password</h1>
+<p class="descripcion-pagina">Reestablece tu password escribiendo tu email a continuacion</p>
+
+<?php
+    include_once __DIR__ . "/../templates/alertas.php";
+?>
+
+<form class="formulario" action="/olvide" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Tu Email"
+        />
+    </div>
+
+    <input type="submit" class="boton" value="Enviar Instrucciones">
+</form>
+
+<div class="acciones">
+    <a href="/">¿Ya tienes una cuenta? Inicia Sesion</a>
+    <a href="/crear-cuenta">¿Aun no tienes una cuenta? Crea Una</a>
+</div>
